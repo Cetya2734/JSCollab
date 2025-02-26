79aa50290567b456c91c12ce9a7e915a272e857e
koma2409 <khang301104@gmail.com>
2025-02-26 21:40:35 +0700

    Scrapped old chest and redoing
    Building a system where the player can opens the chest and interact with the items, Realizing it does not work without a proper inventory system so I have scrapped everything and is now building an inventory system for the player as well

---
+++ Assets/_WorkSeniors/Khang/Scripts/InventoryScripts/InventorySystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+
+[System.Serializable]
+
+public class InventorySystem
+{
+    [SerializeField] private List<InventorySlot> inventorySlots;
+
+    private int inventorySize;
+
+    public List<InventorySlot> InventorySlots => inventorySlots;
+
+    public int InventorySize => InventorySlots.Count;
+
+    public UnityAction<InventorySlot> OnInventorySlotsChanged;
+
+    public InventorySystem(int size)
+    {
+        inventorySlots = new List<InventorySlot>(size);
+
+        for(int i = 0; i < size; i++)
+        {
+            inventorySlots.Add(new InventorySlot());
+        }
+    }
+}
